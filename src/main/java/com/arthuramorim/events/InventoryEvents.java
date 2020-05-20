482889eb552a6279380a7177f8aa6c526f94448f
Arthur paulo <arthurpas67@gmail.com>
2020-05-20 01:20:33 -0300

    update v3.0: Refatoracao dos metodos para tratar caso o jogador nao esta na hashmap, finalizado sistema de evento de compra no shop

--- src/main/java/com/arthuramorim/events/InventoryEvents.java
+++ src/main/java/com/arthuramorim/events/InventoryEvents.java
@@ -3,13 +3,13 @@ package com.arthuramorim.events;
 import com.arthuramorim.Main;
 import com.arthuramorim.enginers.LoadInvAndItems;
 import com.arthuramorim.entity.InvShop;
+import com.arthuramorim.entity.ItemShop;
 import com.arthuramorim.entity.PrestigePlayer;
 import com.arthuramorim.menus.Menus;
 import com.arthuramorim.menus.StaticMenus;
 import com.arthuramorim.utils.Configs;
 import com.arthuramorim.utils.MakeItem;
 import com.arthuramorim.utils.StringColor;
-import com.google.gson.internal.$Gson$Preconditions;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -17,6 +17,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class InventoryEvents implements Listener {
 
@@ -26,40 +27,42 @@ public class InventoryEvents implements Listener {
 
         if (!(p instanceof Player)) return;
 
-
         //inv principal prestigio
         if (e.getInventory().getTitle().contains("Prestigio")) {
-            if (e.getCurrentItem() == null) return;
+            if (e.getCurrentItem() == null) {
+                return;
+            }
             e.setCancelled(true);
             if (e.getClick().isLeftClick()) {
                 if (MakeItem.checkIsSimilar(e.getCurrentItem(), Menus.prestige.build())) {
                     if (p.hasPermission("nero.prestigio")) {
-                        for (PrestigePlayer player : Main.getArrayPlayer()) {
-                            if (player.getUuid().equals(p.getUniqueId())) {
-                                if (player.addPrestige()) {
 
-                                    commandResetRankPlayer(p);
-                                    p.closeInventory();
-                                    p.playSound(p.getLocation(), Sound.ENDERDRAGON_DEATH, 1f, 1f);
-                                    p.sendMessage(StringColor.color("\n&aVoce acabou de fazer prestigio! Parabens!\n"));
+                        PrestigePlayer player = Main.getHashPlayer().get(p.getName());
+                        if (player.addPrestige()) {
 
-                                } else {
-                                    p.sendMessage(StringColor.color("&cOcorreu algum erro e nao foi possivel realizar o prestigio, entre em contato com um staff"));
-                                }
-                            }
+                            commandResetRankPlayer(p);
+                            p.closeInventory();
+                            p.playSound(p.getLocation(), Sound.ENDERDRAGON_DEATH, 1f, 1f);
+                            p.sendMessage(StringColor.color("\n&aVoce acabou de fazer prestigio! Parabens!\n"));
+
+                        } else {
+                            p.sendMessage(StringColor.color("&cOcorreu algum erro e nao foi possivel realizar o prestigio, entre em contato com um staff"));
                         }
 
-                        e.setCancelled(true);
+
                     } else {
                         p.sendMessage(StringColor.color("&cVoce precisa estar no ultimo rank para fazer um prestigio."));
                         p.closeInventory();
+                        return;
                     }
                 }
                 if (e.getCurrentItem().isSimilar(Menus.prestigeShop.build())) {
                     p.openInventory(StaticMenus.createShopInv(LoadInvAndItems.getArrayShopInvs()));
+                    return;
                 }
                 if (e.getCurrentItem().isSimilar(Menus.arrow.build())) {
                     p.closeInventory();
+                    return;
                 }
             } else {
                 e.setCancelled(true);
@@ -68,87 +71,100 @@ public class InventoryEvents implements Listener {
         }
         //inv menu shop categorias
         if (e.getInventory().getTitle().equals(StaticMenus.titlePrestigeShop)) {
-            if (e.getCurrentItem() == null) return;
-            e.setCancelled(true);
-            if (e.getClick().isLeftClick()) {
-                if (!(e.getCurrentItem().isSimilar(Menus.arrow.build()) || e.getCurrentItem().isSimilar(Menus.backgroudPanel.build()) ||
-                        e.getCurrentItem().isSimilar(Menus.seaLantern.build()) || e.getCurrentItem().isSimilar(Menus.web.build()))) {
-                    p.openInventory(StaticMenus.createCategoryShopInv(e.getCurrentItem()));
-                }
-                if (e.getCurrentItem().isSimilar(Menus.arrow.build())) {
-                    p.closeInventory();
-                }
+            if (e.getCurrentItem() == null) {
+                return;
             } else {
                 e.setCancelled(true);
+                try {
+                    if (e.getClick().isLeftClick()) {
+                        if (!(e.getCurrentItem().isSimilar(Menus.arrow.build()) || e.getCurrentItem().isSimilar(Menus.backgroudPanel.build()) ||
+                                e.getCurrentItem().isSimilar(Menus.seaLantern.build()) || e.getCurrentItem().isSimilar(Menus.web.build()))) {
+                            p.openInventory(StaticMenus.createCategoryShopInv(e.getCurrentItem()));
+                            return;
+                        }
+                        if (e.getCurrentItem().isSimilar(Menus.arrow.build())) {
+                            p.closeInventory();
+                            return;
+                        }
+                    }
+                } catch (NullPointerException nullError) {
+                    return;
+                } catch (Exception error) {
+                    error.getStackTrace();
+                    return;
+                }
+            }
+            return;
+        }
+
+        //invs categorias
+        if (e.getInventory().getTitle().equals(StaticMenus.titleInv)) {
+            if (e.getCurrentItem() == null) {
+                return;
+            }
+            e.setCancelled(true);
+            if (e.getCurrentItem().isSimilar(Menus.arrow.build())) {
+                p.closeInventory();
+                return;
+            }
+
+            try {
+                e.setCancelled(true);
+                String[] split = e.getInventory().getTitle().split(StringColor.color("&bCategoria: "));
+                for (InvShop inv : LoadInvAndItems.getArrayShopInvs()) {
+                    if (inv.getName().equals(split[1])) {
+
+                        ItemShop itemShop = inv.getItemsVenda().get(e.getSlot());
+                        PrestigePlayer prestigePlayer = Main.getHashPlayer().get(p.getName());
+
+                        if (prestigePlayer.getPoints() >= itemShop.getPrice()) {
+
+                            if (p.getInventory().firstEmpty() > -1) {
+
+                                if (prestigePlayer.removePoints(itemShop.getPrice())) {
+
+                                    if (itemShop.getOnlyCmd()) {
+                                        itemShop.executCommands(p.getName());
+                                        p.sendMessage(StringColor.color("&aCompra realizada com sucesso!"));
+                                        Main.getAltPlayer().add(prestigePlayer);
+
+                                        return;
+                                    } else {
+                                        if (itemShop.getCmds() != null) {
+                                            itemShop.executCommands(p.getName());
+                                            p.getInventory().addItem(itemShop.getItems());
+                                            p.sendMessage(StringColor.color("&aCompra realizada com sucesso!"));
+                                            Main.getAltPlayer().add(prestigePlayer);
+                                            return;
+                                        } else {
+                                            p.getInventory().addItem(itemShop.getItems());
+                                            p.sendMessage(StringColor.color("&aCompra realizada com sucesso!"));
+                                            Main.getAltPlayer().add(prestigePlayer);
+                                            return;
+                                        }
+                                    }
+                                } else {
+                                    p.sendMessage(StringColor.color("&cAlguma coisa nao esta certa... Tente de novo ou fale com um staff."));
+                                    return;
+                                }
+                            } else {
+                                p.sendMessage(StringColor.color("&cVoce deve ter pelo menos 1 slot do inventario livre."));
+                                return;
+                            }
+                        } else {
+                            p.sendMessage(StringColor.color("&cVoce nao possui ppoints suficiente."));
+                            return;
+                        }
+                    }
+                    break;
+                }
+            } catch (Exception err) {
                 return;
             }
         }
-        //invs categorias
-        if (e.getInventory().getTitle().contains(StringColor.color("&bCategoria: "))) {
-            if (e.getCurrentItem() == null) return;
-            e.setCancelled(true);
-            if ((e.getSlot() > 9 && e.getSlot() < 17) ||
-                    (e.getSlot() > 18 && e.getSlot() < 25) ||
-                    (e.getSlot() > 27 && e.getSlot() < 35) ||
-                    (e.getSlot() > 36 && e.getSlot() < 44)) {
 
-                if(e.getCurrentItem() == null){
-                    return;
-                }
-//erro ao clicar fora do inv
-                for (InvShop inv : LoadInvAndItems.getArrayShopInvs()) {
-                    if (inv.getName().contains(e.getInventory().getTitle().split(StringColor.color("&bCategoria: "))[1])) {
-                        if(e.getCurrentItem().equals(Material.AIR) || e.getCurrentItem() == null) return;
-//                        if(e.getCurrentItem() != inv.getItemsVenda().get(e.getSlot()).getItems()) return;
-//                        for (PrestigePlayer player : Main.getArrayPlayer()) {
-//                            if (player.getUuid().equals(p.getUniqueId())) {
-//
-//                                if (player.getPoints() >= inv.getItemsVenda().get(e.getSlot()).getPrice()) {
-//                                    if (inv.getItemsVenda().get(e.getSlot()).getOnlyCmd()) {
-//                                        if (player.removePoints(inv.getItemsVenda().get(e.getSlot()).getPrice())) {
-//                                            inv.getItemsVenda().get(e.getSlot()).executCommands(p.getName());
-//                                            p.sendMessage(StringColor.color("&aCompra realizada com sucesso!"));
-//                                        } else {
-//                                            p.sendMessage(StringColor.color("&cOcorreu um erro ao processar a compra. Entre em contato com um staff!"));
-//                                        }
-//                                    } else {
-//                                        if (player.removePoints(inv.getItemsVenda().get(e.getSlot()).getPrice())) {
-//                                            p.getInventory().addItem(inv.getItemsVenda().get(e.getSlot()).getItems());
-//                                            inv.getItemsVenda().get(e.getSlot()).executCommands(p.getName());
-//                                            p.sendMessage(StringColor.color("&aCompra realizada com sucesso!"));
-//                                        } else {
-//                                            p.sendMessage(StringColor.color("&cVoce nao possui pontos de prestigio suficiente."));
-//                                        }
-//                                    }
-//                                }
-//
-//                                break;
-//                            }
-//                        }
-//
-//                        break;
-                    }
-                }
-                e.setCancelled(true);
 
-                e.setCancelled(true);
-
-            } else if (e.getCurrentItem().isSimilar(Menus.arrow.build())) {
-                e.setCancelled(true);
-                p.closeInventory();
-            } else {
-                e.setCancelled(true);
-            }
-        } else {
-            return;
-        }
-    }
-
-    @EventHandler
-    public void invShopCategory(InventoryClickEvent e){
-
-        if(e.getCurrentItem() == null) return;
-
+        return;
     }
 
 
