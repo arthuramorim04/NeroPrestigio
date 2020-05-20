33634fbd125d4c07776ddc55949df093c9359948
Arthur paulo <arthurpas67@gmail.com>
2020-05-20 01:28:27 -0300

    update v3.0: adicionado task para salvar players que tiveram alt ou quando o plugin e desabilitado

---
+++ src/main/java/com/arthuramorim/taks/SavePlayer.java
@@ -0,0 +1,26 @@
+package com.arthuramorim.taks;
+
+import com.arthuramorim.Main;
+import com.arthuramorim.controllers.PlayerController;
+import com.arthuramorim.entity.PrestigePlayer;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class SavePlayer extends BukkitRunnable {
+
+    @Override
+    public void run() {
+
+        saveAll();
+    }
+
+    public void saveAll(){
+        if(Main.getAltPlayer().isEmpty()) return;
+
+        for (PrestigePlayer p : Main.getAltPlayer()) {
+            PlayerController.savePlayerTask(p);
+        }
+
+        Main.getAltPlayer().clear();
+    }
+
+}
